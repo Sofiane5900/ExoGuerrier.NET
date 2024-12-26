18025dcdfd3bc06121737e0c0ac65ee52afabb21
Sufyān <132440690+Sofiane5900@users.noreply.github.com>
2024-12-26 12:07:29 +0100

    ajout redefinition Attaquer pour Elfe

---
+++ ExoGuerrier.NET/Elfe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExoGuerrier.NET
+{
+    internal class Elfe : Guerrier
+    {
+        public Elfe(string Nom, int PointsDeVie, int NbDesAttaque)
+            : base(Nom, PointsDeVie, NbDesAttaque) { }
+
+        public int Attaquer()
+        {
+            int totalDegats = 0;
+            totalDegats = Math.Max(totalDegats, NbDesAttaque);
+            return totalDegats;
+        }
+    }
+}
